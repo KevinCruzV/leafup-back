9dcbc068a76a22b8016a68a13f2c96718e298c2b
KevinCruzV <72854509+KevinCruzV@users.noreply.github.com>
2022-07-07 15:54:29 +0200

    recup arduino var et request

--- backend/liaison-arduino/test.py
+++ backend/liaison-arduino/test.py
@@ -1,5 +1,8 @@
 import pyfirmata
 import time
+import requests
+import time
+
 
 port = '/dev/cu.usbserial-10'  # Windows (à adapter par rapport à votre ordinateur)
 board = pyfirmata.Arduino(port)  # Permet d’ouvrir le port associer
@@ -13,9 +16,32 @@ while temperature_pin.read() is None: var = None # Tant qu’il n’y a pas de v
 
 try:
     while True:  # Boucle infinie
-        print("température entre 0 et 1 :", temperature_pin.read())  # On affiche les valeurs lues par la broche
-        Celsius = ((temperature_pin.read() * 5 - 0.5) * 100)  # Transforme ces valeurs en Celsius
-        print("température en Celsius :", (round(Celsius, 4)))  # Arrondit et affiche la température
+        print("Celsius :", temperature_pin.read())  # On affiche les valeurs lues par la broche
+        #Celsius = ((temperature_pin.read() * 5 - 0.5) * 100)  # Transforme ces valeurs en Celsius
+        #print("température en Celsius :", (round(Celsius, 4)))  # Arrondit et affiche la température
+
+        if temperature_pin.read() > 0 :
+
+            url = "http://localhost:8000/api/plants/"
+            headers = {"Content-Type": "application/json;"}
+
+            # A remplacer par la valeur du capteur
+            temp = temperature_pin.read()
+
+            data = {
+                "name": "test plante",
+                "created_at": "2022-07-7T01:51:42.000Z",
+                "species": "1",
+                "last_watering": "2022-07-7T01:51:42.000Z",
+                "temp": temp
+            }
+
+            response = requests.post(url, headers=headers, json=data)
+
+            while True:
+                print("Status Code: ", response.status_code)
+                time.sleep(5)
+
         time.sleep(1)  # Fait une pause de 1 seconde entre deux mesures
 except:
     temperature_pin.disable_reporting()  # Arrête la lecture de la broche
